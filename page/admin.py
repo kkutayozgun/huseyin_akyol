29aa1a1b633bfca5ebeb91bd89ea2a2ec26919e3
gocamancupcake <calilmande@gmail.com>
2022-07-02 21:44:35 +0300

    wip: all other models and admin registry

--- page/admin.py
+++ page/admin.py
@@ -3,11 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 from parler.admin import TranslatableAdmin
 
 from page.models import (Keywords, seo_translations, HomePageSeo, About, AboutList, Image,
-                         HomePageSeoSlides, AboutSlides, AboutBottomSlides)
+                         HomePageSeoSlides, AboutSlides, AboutBottomSlides,
+                         Surgery, ChangeJourney, TreatmentPlan, TreatmentPlanItems, Faq
+                         )
 
 admin.site.register(Keywords, TranslatableAdmin)
 admin.site.register(Image)
 admin.site.register(AboutList)
+admin.site.register(TreatmentPlanItems)
 
 seo_fields = tuple(seo_translations.keys()) + ("meta_keywords",)
 
@@ -36,3 +39,24 @@ class AboutSlidesInline(admin.TabularInline):
 class AboutAdmin(TranslatableAdmin):
     exclude = ('slides', 'slides_bottom')
     inlines = [AboutListInline, AboutSlidesInline, AboutSlidesBottomInline]
+
+
+@admin.register(Surgery)
+class SurgeryAdmin(TranslatableAdmin):
+    pass
+
+@admin.register(Faq)
+class FaqAdmin(TranslatableAdmin):
+    pass
+
+@admin.register(ChangeJourney)
+class ChangeJourneyAdmin(admin.ModelAdmin):
+    pass
+
+
+class TreatmentPlanItemsInline(admin.TabularInline):
+    model = TreatmentPlanItems
+
+@admin.register(TreatmentPlan)
+class TreatmentPlanAdmin(TranslatableAdmin):
+    inlines = [TreatmentPlanItemsInline]
--- page/models/home.py
+++ page/models/home.py
@@ -40,11 +40,9 @@ class About(TranslatableModel):
         verbose_name = _("Hakkımızda")
         verbose_name_plural = _("Hakkımızda")
 
-class AboutList(TranslatableModel):
+class AboutList(models.Model):
     about = models.ForeignKey(About, verbose_name=_("Hakkımızda"), on_delete=models.CASCADE)
-    translations = TranslatedFields(
-        item=models.CharField(_("Madde"), default="", blank=False, max_length=25),
-    )
+    item=models.CharField(_("Madde"), default="", blank=False, max_length=25)
 
     class Meta:
         verbose_name = _("Hakkımızda Maddeler")
@@ -62,3 +60,51 @@ class AboutSlides(models.Model):
 class AboutBottomSlides(models.Model):
     about=models.ForeignKey(About, related_name="about_slides_bottom", on_delete=models.CASCADE)
     image=models.ForeignKey(Image, on_delete=models.CASCADE)
+
+
+class Surgery(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.CharField(_("Başlık"), default="", blank=True, max_length=80),
+        description=models.TextField(_("Açıklama"), default="", blank=True),
+        image = models.ImageField(_("Resim"), upload_to="surgery", blank=False)
+    )
+
+    class Meta:
+        verbose_name = _("Obezite ve Metabolik Cerrahi")
+        verbose_name_plural = _("Obezite ve Metabolik Cerrahi")
+
+
+class ChangeJourney(models.Model):
+    image = models.ImageField(_("Resim"), upload_to="change-journey", blank=False)
+
+    class Meta:
+        verbose_name = _("Değişim Yolculuğu")
+        verbose_name_plural = _("Değişim Yolculukları")
+
+
+class TreatmentPlan(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(_("Adı"), blank=False, max_length=25),
+    )
+
+    class Meta:
+        verbose_name = _("Tedavi Planı")
+        verbose_name_plural = _("Tedavi Planları")
+
+class TreatmentPlanItems(models.Model):
+    treatment_plan = models.ForeignKey(TreatmentPlan, verbose_name=_("Tedavi Planı"), on_delete=models.CASCADE)
+    item=models.CharField(_("Madde"), blank=False, max_length=25)
+
+    class Meta:
+        verbose_name = _("Tedavi Planı Maddeler")
+        verbose_name_plural = _("Tedavi Planı Maddeler")
+
+
+class Faq(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.CharField(_("Soru"), blank=False, max_length=85),
+        description=models.TextField(_("Cevap"), blank=False),
+    )
+    class Meta:
+        verbose_name = _("Sık Sorulan Soru")
+        verbose_name_plural = _("Sık Sorulan Sorular")
