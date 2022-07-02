f232bac037b6770b32827a554f168e939f1ff2f4
gocamancupcake <calilmande@gmail.com>
2022-07-02 20:38:49 +0300

    wip: home intro block, about, sliders

--- page/admin.py
+++ page/admin.py
@@ -2,16 +2,37 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from parler.admin import TranslatableAdmin
 
-from page.models import (Keywords, seo_translations, HomePageSeo)
+from page.models import (Keywords, seo_translations, HomePageSeo, About, AboutList, Image,
+                         HomePageSeoSlides, AboutSlides, AboutBottomSlides)
 
 admin.site.register(Keywords, TranslatableAdmin)
+admin.site.register(Image)
+admin.site.register(AboutList)
 
 seo_fields = tuple(seo_translations.keys()) + ("meta_keywords",)
 
+class HomeSlidesInline(admin.TabularInline):
+    model = HomePageSeoSlides
 
 @admin.register(HomePageSeo)
 class HomePageSeoAdmin(TranslatableAdmin):
     fieldsets = (
         (_("Seo Information"), {'fields': seo_fields}),
     )
-    filter_vertical = ('meta_keywords',)
+    inlines = [HomeSlidesInline]
+    exclude = ('slides',)
+
+
+class AboutListInline(admin.TabularInline):
+    model = AboutList
+
+class AboutSlidesBottomInline(admin.TabularInline):
+    model = AboutSlides
+
+class AboutSlidesInline(admin.TabularInline):
+    model = AboutBottomSlides
+
+@admin.register(About)
+class AboutAdmin(TranslatableAdmin):
+    exclude = ('slides', 'slides_bottom')
+    inlines = [AboutListInline, AboutSlidesInline, AboutSlidesBottomInline]
--- page/models/home.py
+++ page/models/home.py
@@ -5,11 +5,60 @@ from parler.models import TranslatableModel, TranslatedFields
 from page.models import seo_translations, SEOStarterModel
 
 
+class Image(models.Model):
+    image = models.ImageField(_("Resim"), upload_to="images", blank=False)
+
+    class Meta:
+        verbose_name = _("Resim")
+        verbose_name_plural = _("Resimler")
+
 class HomePageSeo(TranslatableModel, SEOStarterModel):
+    slides = models.ManyToManyField(Image, related_name="intro_slides", through="HomePageSeoSlides")
     translations = TranslatedFields(
-        **seo_translations
+        **seo_translations,
+        title_intro=models.CharField(_("Başlık Girişi"), default="", blank=True, max_length=60),
+        title=models.CharField(_("Başlık"), default="", blank=True, max_length=80),
+        description=models.TextField(_("Açıklama"), default="", blank=True),
+        bottom_content=models.TextField(_("Alt Bant İçeriği"), default="", blank=True),
     )
 
     class Meta:
-        verbose_name = _("Anasayfa SEO")
-        verbose_name_plural = _("Anasayfa SEO")
+        verbose_name = _("Anasayfa Giriş Bloku")
+        verbose_name_plural = _("Anasayfa Giriş Bloku")
+
+class About(TranslatableModel):
+    slides = models.ManyToManyField(Image, related_name="about_slides", through="AboutSlides")
+    slides_bottom = models.ManyToManyField(Image, related_name="about_slides_bottom", through="AboutBottomSlides")
+    translations = TranslatedFields(
+        title=models.CharField(_("Başlık"), default="", blank=True, max_length=80),
+        intro=models.TextField(_("Giriş"), default="", blank=True),
+        description_title=models.CharField(_("Açıklama Girişi"), default="", blank=True, max_length=80),
+        description=models.TextField(_("Açıklama"), default="", blank=True),
+    )
+
+    class Meta:
+        verbose_name = _("Hakkımızda")
+        verbose_name_plural = _("Hakkımızda")
+
+class AboutList(TranslatableModel):
+    about = models.ForeignKey(About, verbose_name=_("Hakkımızda"), on_delete=models.CASCADE)
+    translations = TranslatedFields(
+        item=models.CharField(_("Madde"), default="", blank=False, max_length=25),
+    )
+
+    class Meta:
+        verbose_name = _("Hakkımızda Maddeler")
+        verbose_name_plural = _("Hakkımızda Maddeler")
+
+# slide relations
+class HomePageSeoSlides(models.Model):
+    home=models.ForeignKey(HomePageSeo, on_delete=models.CASCADE)
+    image=models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class AboutSlides(models.Model):
+    about=models.ForeignKey(About, related_name="about_slides", on_delete=models.CASCADE)
+    image=models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class AboutBottomSlides(models.Model):
+    about=models.ForeignKey(About, related_name="about_slides_bottom", on_delete=models.CASCADE)
+    image=models.ForeignKey(Image, on_delete=models.CASCADE)
