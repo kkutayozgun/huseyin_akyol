70050425f03744632008ff4b4e9f71a44c7fd74a
Kutay Ozgun <kkutayozgun@gmail.com>
2022-07-15 10:11:53 +0300

    changed email submissions

--- page/views.py
+++ page/views.py
@@ -9,7 +9,7 @@ class KVKKPage(TemplateView):
     template_name = "kvkk.html"
 
 class ContactFormEmailView(HandleEmailFormView):
-    subject = "DentalBosphorus - İletişim Sayfası formu dolduruldu"
+    subject = "Dr. Hüseyin Akyol - İletişim Sayfası formu dolduruldu: {first_name} {last_name}"
     email_template_name = "emailtemps/contact_form.html"
     form_identifier = "contact-form"
 
@@ -19,6 +19,7 @@ class ContactFormEmailView(HandleEmailFormView):
             "last_name": request.POST.get('last_name', ""),
             "email": request.POST.get('email', ""),
             "treatment": request.POST.get('treatment', ""),
+            "full_phone_number": request.POST.get('contact-full_number', ""),
             "phone": request.POST.get('phone', ""),
             "via": request.POST.get('via', ""),
             "message": request.POST.get('message', "")
